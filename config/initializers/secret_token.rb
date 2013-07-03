a1811c1cc518afc9bea79105f51af631c89514c4
Aaron Wheeler <aaron@moz.com>
2013-07-02 18:13:29 -0700

    Generate the Rails 4 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ticketee::Application.config.secret_key_base = 'c5c4adb3e84a5e45caa423d31bd151db37d47c3a582ac74af8bf15062d839d93fec497274f188d21f4bd899f48136bd2dcafce82b5cee94ffc5cf53887f672d6'
